fca45336edec3e94c6872dd831ca604a0577a805
Aman <amanxyz.10may@gmail.com>
2026-04-27 08:04:50 +0530

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,18 @@
+interface Animal{
+    public void walk();
+
+}
+
+class Horse implements Animal{
+   public void walk(){
+    System.out.println("walks on 4 leg");
+   }
+
+}
+
+public class Interface {
+ public static void main(String[] args) {
+    Horse horse = new Horse();
+    horse.walk();
+ }
+}
